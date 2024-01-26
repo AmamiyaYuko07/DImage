cbe39aa5d606d1e2d7fc62160c195f11584f7df8
Amamiya <1069381036@qq.com>
2024-01-26 16:06:41 +0800

    添加项目文件。

---
+++ ImageHelper.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DImage
+{
+    public class ImageHelper
+    {
+        public static void DImage(string filepath, Encoding encoding, string output)
+        {
+            var header = new byte[] { 0x50, 0x4B, 0x03, 0x04 };
+            using (var stream = File.OpenRead(filepath))
+            {
+                var buffer = new byte[4];
+                var goNext = false;
+                var index = 0L;
+                do
+                {
+                    stream.Seek(index, SeekOrigin.Begin);
+                    if (stream.ReadByte() == header[0])
+                    {
+                        if (stream.ReadByte() == header[1])
+                        {
+                            if (stream.ReadByte() == header[2])
+                            {
+                                if (stream.ReadByte() == header[3])
+                                {
+                                    index = stream.Position - 4;
+                                    goNext = true;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    index++;
+                } while (!goNext);
+                stream.Seek(index, SeekOrigin.Begin);
+                var path = Path.Combine(AppContext.BaseDirectory, output);
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    stream.CopyTo(ms);
+                    ms.Seek(0, SeekOrigin.Begin);
+                    using (ZipArchive zip = new ZipArchive(ms, ZipArchiveMode.Read, true, encoding))
+                    {
+                        foreach (var entry in zip.Entries)
+                        {
+                            entry.ExtractToFile(Path.Combine(path, entry.Name), true);
+                        }
+                    }
+                }
+            }
+        }
+
+        public static void EImage(string image, string[] files, string output, Encoding encoding)
+        {
+            var path = Path.Combine(AppContext.BaseDirectory, output);
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            var outfile = Path.Combine(AppContext.BaseDirectory, output, Path.GetFileName(image));
+            using (var outputStream = new FileStream(outfile, FileMode.Create, FileAccess.Write))
+            {
+                using (var imageStream = File.OpenRead(image))
+                {
+                    imageStream.CopyTo(outputStream);
+                }
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (ZipArchive zip = new ZipArchive(ms, ZipArchiveMode.Create, true, encoding))
+                    {
+                        foreach (var file in files)
+                        {
+                            var filename = Path.GetFileName(file);
+                            zip.CreateEntryFromFile(file, filename);
+                        }
+                    }
+                    ms.Seek(0, SeekOrigin.Begin);
+                    ms.CopyTo(outputStream);
+                }
+                outputStream.Flush();
+                outputStream.Close();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using DImage;
+using System.CommandLine;
+using System.IO.Compression;
+using System.Text;
+
+Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+var rootCommand = new RootCommand("Compress/UnCompress File On Png Image");
+
+var decodeCommand = new Command("d", "UnCompress File On Png Image");
+var imageOption = new Option<string>(name: "--image", description: "Png Image File") { IsRequired = true };
+imageOption.AddAlias("-i");
+var encodeOption = new Option<string>(name: "--encode", description: "FileName Encode", getDefaultValue: () => "UTF-8");
+encodeOption.AddAlias("-e");
+var outputOption = new Option<string>(name: "--output", description: "Output File", getDefaultValue: () => "outputs");
+outputOption.AddAlias("-o");
+var filesOption = new Option<string[]>(name: "--file", description: "Target File") { IsRequired = true };
+filesOption.AddAlias("-f");
+var passwordOption = new Option<string>(name: "--password", description: "Password");
+passwordOption.AddAlias("-p");
+decodeCommand.AddOption(imageOption);
+decodeCommand.AddOption(encodeOption);
+decodeCommand.AddOption(outputOption);
+decodeCommand.AddOption(passwordOption);
+decodeCommand.SetHandler((image, encode, output) =>
+{
+    if (!File.Exists(image))
+    {
+        Console.WriteLine("Png file not found！");
+        return;
+    }
+    ImageHelper.DImage(image, Encoding.GetEncoding(encode), output);
+}, imageOption, encodeOption, outputOption);
+
+var encodeCommand = new Command("e", "Compress File Into Png Image");
+encodeCommand.AddOption(imageOption);
+encodeCommand.AddOption(filesOption);
+encodeCommand.AddOption(encodeOption);
+encodeCommand.AddOption(outputOption);
+encodeCommand.AddOption(passwordOption);
+encodeCommand.SetHandler((image, files, output, encode) =>
+{
+    if (!File.Exists(image))
+    {
+        Console.WriteLine("Png file not found！");
+        return;
+    }
+    foreach (var file in files)
+    {
+        if (!File.Exists(file))
+        {
+            Console.WriteLine($"file:{file} not found！");
+            return;
+        }
+    }
+    ImageHelper.EImage(image, files, output, Encoding.GetEncoding(encode));
+}, imageOption, filesOption, outputOption, encodeOption);
+rootCommand.Add(decodeCommand);
+rootCommand.Add(encodeCommand);
+await rootCommand.InvokeAsync(args);
